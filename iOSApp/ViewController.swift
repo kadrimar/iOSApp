259af64e7b68acda80c83447b186947346f38c60
kasutaja <kasutaja@s244-10.cs.tlu.ee>
2015-03-12 12:29:26 +0200

    Initial Commit

---
+++ iOSApp/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  iOSApp
+//
+//  Created by kasutaja on 12/03/15.
+//  Copyright (c) 2015 kasutaja. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
